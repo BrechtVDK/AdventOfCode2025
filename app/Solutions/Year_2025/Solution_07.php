a4e38113e73842bd021bab425d49372090070a40
BrechtVDK <brechtvdk27@gmail.com>
2025-12-08 08:33:09 +0100

    2025 6 7  optimize

--- app/Solutions/Year_2025/Solution_07.php
+++ app/Solutions/Year_2025/Solution_07.php
@@ -26,24 +26,6 @@ class Solution_07 extends AbstractSolution
 
     public function silver(): int|string
     {
-        $startPoint = [0, array_search('S', $this->grid[0])];
-        $this->startingPointsToDoStack[] = $startPoint;
-        do {
-            $startPoint = array_shift($this->startingPointsToDoStack);
-            if (array_search($startPoint, $this->startingPointsDone)) {
-                continue;
-            }
-
-            $this->beamSilver($startPoint);
-
-        } while (!empty($this->startingPointsToDoStack));
-        //$this->printGrid();
-        return $this->countSplits();
-    }
-
-    public function gold(): int|string
-    {
-        $this->grid = $this->parseInput();
         for ($row = 0; $row < count($this->grid); $row++) {
             for ($col = 0; $col < count($this->grid[$row]); $col++) {
                 if ($this->grid[$row][$col] === '.') {
@@ -51,37 +33,30 @@ class Solution_07 extends AbstractSolution
                 }
             }
         }
-
         $startPoint = [0, array_search('S', $this->grid[0])];
         $this->grid[0][$startPoint[1]] = 1;
 
-        $this->beamGold();
+        $this->beam();
+        return $this->countSplits();
+
+    }
+
+    public function gold(): int|string
+    {
 
         return array_sum($this->grid[count($this->grid) - 1]);
 
     }
 
-    private function printGrid(): void
-    {
-        $print = '';
-        foreach ($this->grid as $row) {
-            foreach ($row as $cell) {
-                $print .= str_pad($cell === self::SPLITTER ? 'i' : $cell, '4');
-            }
-            $print .= PHP_EOL;
-        }
-        ray()->text($print);
-    }
-
-    private function beamGold(): void
+    private function beam(): void
     {
         for ($row = 1; $row < count($this->grid); $row++) {
             for ($col = 0; $col < count($this->grid[$row]); $col++) {
                 if ($this->grid[$row][$col] === self::SPLITTER) {
-                    $this->grid[$row][$col - 1] +=  $this->grid[$row - 1][$col];
-                    $this->grid[$row][$col + 1] +=   $this->grid[$row - 1][$col];
+                    $this->grid[$row][$col - 1] += $this->grid[$row - 1][$col];
+                    $this->grid[$row][$col + 1] += $this->grid[$row - 1][$col];
                 } elseif ($this->grid[$row - 1][$col] !== self::SPLITTER) {
-                    $this->grid[$row][$col] +=  $this->grid[$row - 1][$col];
+                    $this->grid[$row][$col] += $this->grid[$row - 1][$col];
                 }
             }
         }
@@ -89,37 +64,12 @@ class Solution_07 extends AbstractSolution
 
     }
 
-    private
-    function beamSilver(array $startPoint): void
-    {
-        $col = $startPoint[1];
-        for ($row = $startPoint[0]; $row < count($this->grid); $row++) {
-            if ($this->grid[$row][$col] === self::SPLITTER) {
-                if ($col > 0) {
-                    $this->startingPointsToDoStack[] = [$row, $col - 1];
-                    $this->grid[$row][$col - 1] = self::BEAM;
-
-                }
-                if ($col < count($this->grid[$row]) - 1) {
-                    $this->startingPointsToDoStack[] = [$row, $col + 1];
-                    $this->grid[$row][$col + 1] = self::BEAM;
-                }
-
-                break;
-            } else {
-                $this->grid[$row][$col] = self::BEAM;
-            }
-        }
-        $this->startingPointsDone[] = $startPoint;
-    }
-
-    private
-    function countSplits(): int
+    private function countSplits(): int
     {
         $splits = 0;
         for ($row = 0; $row < count($this->grid); $row++) {
             for ($col = 0; $col < count($this->grid[$row]); $col++) {
-                if ($this->grid[$row][$col] === self::SPLITTER && $this->grid[$row][$col - 1] === self::BEAM && $this->grid[$row][$col + 1] === self::BEAM && $this->grid[$row - 1][$col] === self::BEAM) {
+                if ($this->grid[$row][$col] === self::SPLITTER && $this->grid[$row][$col - 1] > 0 && $this->grid[$row][$col + 1] > 0 && $this->grid[$row - 1][$col] > 0) {
                     $splits++;
                 }
             }
